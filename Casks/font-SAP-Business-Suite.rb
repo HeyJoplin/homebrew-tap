788646e92eabce7356956bca9a96d829d5211847
HeyJoplin <2277648+HeyJoplin@users.noreply.github.com>
2021-11-12 16:02:48 +0100

    New Casks for SAP fonts

---
+++ Casks/font-SAP-Business-Suite.rb
@@ -0,0 +1,11 @@
+cask "font-SAP-Business-Suite" do
+    version "1.073"
+    sha256 :no_check
+  
+    url "https://experience.sap.com/fiori-design-web/wp-content/uploads/sites/5/2021/07/BusinessSuite-Icons.zip"
+    name "SAP Business Suite Icon Font"
+    desc "This icon font contains application-specific icons primarily used in SAP S/4HANA apps"
+    homepage "https://experience.sap.com/fiori-design-web/downloads/#download-icon-font"
+  
+    font "BusinessSuite-Icons/IconHTMLTemplate_InAppSymbols_1.073/fonts/BusinessSuiteInAppSymbols.ttf"
+  end
---
+++ Casks/font-SAP-Icon.rb
@@ -0,0 +1,11 @@
+cask "font-SAP-Icon" do
+    version "4.10"
+    sha256 :no_check
+  
+    url "https://experience.sap.com/fiori-design-web/wp-content/uploads/sites/5/2021/07/SAP-icons_4.10.zip"
+    name "SAP Icon Fonts"
+    desc "SAP Icon Fonts"
+    homepage "https://experience.sap.com/fiori-design-web/downloads/#download-icon-font"
+  
+    font "SAP-icons_4.10/Fonts/SAP-icons.ttf"
+  end
